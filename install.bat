8c15868aa3eb14af960d503a0161893920521e54
thegooster <gooingben@gmail.com>
2024-05-26 22:08:59 -0700

    v1.1

--- install.bat
+++ install.bat
@@ -1,18 +1,5 @@
 @echo off
-REM Create a virtual environment named 'venv'
-python -m venv venv
-
-REM Activate the virtual environment
-call venv\Scripts\activate
-
-REM Install packages from requirements.txt
 python -m pip install -r requirements.txt
-
-REM Run the main.py script
 python scripts\main.py
-
-REM Deactivate the virtual environment
-call venv\Scripts\deactivate
-
 echo Installation and execution completed.
 pause
--- run.bat
+++ run.bat
@@ -1,12 +1,3 @@
 @echo off
-REM Activate the virtual environment
-call venv\Scripts\activate
-
-REM Run the main.py script
-python \scripts\main.py
-
-REM Deactivate the virtual environment
-call venv\Scripts\deactivate
-
-echo Execution completed.
+python scripts\main.py
 pause
